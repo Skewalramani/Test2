6ba20c1aba0c7b7a5179096520157265e38113d7
Skewalramani <sagar.kewalramani@gmail.com>
2015-07-03 18:44:50 +0530

    Updated Script

--- copy.sh
+++ copy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/usr/bin/scp -i /home/ec2-user/.ssh/RadianDataProfiler.pem  /var/lib/jenkins/jobs/Automation/workspace/mdm.sql ec2-user@ec2-52-6-180-136.compute-1.amazonaws.com:/home/ec2-user/
+/usr/bin/scp -i RadianDataProfiler.pem  /var/lib/jenkins/jobs/Automation/workspace/mdm.sql ec2-user@ec2-52-6-180-136.compute-1.amazonaws.com:/home/ec2-user/
 echo "Hello World"
